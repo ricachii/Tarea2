cd4817f7150e4a41f364bf1fa2fad5590567bebb
Ricachi <nicolasricciardi2002@gmail.com>
2022-10-25 21:06:17 -0100

    Creacion del paquete Objetos

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tarea2;
 
-/**
- *
- * @author Nicolas
- */
-public class Tarea2 {
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Tarea2 {  
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+       
+        
+    }   
 }
